6e6d219d6d3b7c39de78b3d51dec6f735dfb315c
christopherdunn <cdunn@backbayrealtygroup.com>
2021-09-13 18:05:06 -0400

    updated model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :artworks
 
-  validates_presence_of :promo_code
-
-
+  validates_presence_of :promo_code, format: { with: /[A-Z]{3}\d{4}|[A-Z]{4}\d{4}/}  
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
